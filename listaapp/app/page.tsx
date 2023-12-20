a9b8190748bd25f2927487cbf5d1baca74b793ad
MonicaNeve <138527753+MonicaNeve@users.noreply.github.com>
2023-12-20 10:22:17 -0300

    app

---
+++ listaapp/app/page.tsx
@@ -0,0 +1,22 @@
+type Country = {
+  name: {
+    common: string;
+  }
+}
+
+async function getCountries(): Promise<country[]> {
+  const response = await fetch("https://restcountries.com/v3.1/all")
+  return response.json();
+}
+
+export default async function Home() {
+  const countries = await getCountries();
+
+  // console.log(countries)
+  return (
+    <section className='container flex w-full'>
+      {countries.map((country)=>(<h1 key={country.name.common}>{country.name.common}</h1>))}
+
+    </section>
+  )
+}
